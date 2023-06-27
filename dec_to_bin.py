abdd1256391c2e3b4913371bf26e413ddc4e613c
pnetocny <petrnetocny@gmail.com>
2023-06-27 23:34:11 +0200

    Doplnění kontroly o případ, že by číslo začínalo znaménkem "+"

--- dec_to_bin.py
+++ dec_to_bin.py
@@ -1,6 +1,17 @@
-bin_cislo = ("")
-dec_cislo = input("Zadej celé kladné číslo (bez znaménka ´+´ pro převedení do binární soustavy: ")
-if dec_cislo.isdigit() and int (dec_cislo) > 0:
+print()
+
+# nastavení výchozí hodnoty (prázdný řetězec) pro binární číslo
+bin_cislo = ("") 
+
+# vložení čísla z klávesnice
+dec_cislo = input("Zadej celé kladné číslo pro převedení do binární soustavy: ")
+
+# rozdělení řetězce na první znak a ostatní znaky (pro následnou kontrolu)
+prvni_znak = dec_cislo[0]
+ostatni_znaky = "0" + dec_cislo[1:] 
+
+if (prvni_znak == "+" or prvni_znak.isdigit()) and ostatni_znaky.isdigit():
+    
     dec_cislo = int(dec_cislo) 
     while dec_cislo > 0:
         bin_pom = dec_cislo % 2 
@@ -9,3 +20,4 @@ if dec_cislo.isdigit() and int (dec_cislo) > 0:
     print ("Číslo převedené do binární soustavy je: ", bin_cislo)
 else: 
     print ("Neplatný vstup. Zadej prosím celé kladné číslo.")
+print()

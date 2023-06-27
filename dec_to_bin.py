d034471835be94d2cb25b34274ba8eca4632a771
pnetocny <petrnetocny@gmail.com>
2023-06-27 12:05:17 +0200

    Přidání kontroly zda bylo zadáno kladné číslo

--- dec_to_bin.py
+++ dec_to_bin.py
@@ -1,8 +1,11 @@
 bin_cislo = ("")
-bin_pom = 0
-dec_cislo = int(input("Zadej celé kladné číslo pro převedení do binární soustavy: "))
-while dec_cislo > 0:
-    bin_pom = dec_cislo % 2 
-    bin_cislo = str (bin_pom) + bin_cislo
-    dec_cislo = dec_cislo // 2
-print ("Číslo převedené do binární soustavy je: ", bin_cislo)
+dec_cislo = input("Zadej celé kladné číslo (bez znaménka ´+´ pro převedení do binární soustavy: ")
+if dec_cislo.isdigit() and int (dec_cislo) > 0:
+    dec_cislo = int(dec_cislo) 
+    while dec_cislo > 0:
+        bin_pom = dec_cislo % 2 
+        bin_cislo = str (bin_pom) + bin_cislo
+        dec_cislo = dec_cislo // 2
+    print ("Číslo převedené do binární soustavy je: ", bin_cislo)
+else: 
+    print ("Neplatný vstup. Zadej prosím celé kladné číslo.")

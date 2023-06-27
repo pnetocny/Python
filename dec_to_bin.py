2025e3e896884bae4f243c09f4490ce3dd2adb96
pnetocny <petrnetocny@gmail.com>
2023-06-27 23:36:06 +0200

    Doplnění komentářů

--- dec_to_bin.py
+++ dec_to_bin.py
@@ -8,16 +8,18 @@ dec_cislo = input("Zadej celé kladné číslo pro převedení do binární sous
 
 # rozdělení řetězce na první znak a ostatní znaky (pro následnou kontrolu)
 prvni_znak = dec_cislo[0]
-ostatni_znaky = "0" + dec_cislo[1:] 
+ostatni_znaky = "0" + dec_cislo[1:] # přidání "0" aby proměnná ostatni_znaky nebyla prázdná, kdyby byl na vstupu zadán jen jeden znak (např. číslo 5) 
 
-if (prvni_znak == "+" or prvni_znak.isdigit()) and ostatni_znaky.isdigit():
+if (prvni_znak == "+" or prvni_znak.isdigit()) and ostatni_znaky.isdigit(): 
     
     dec_cislo = int(dec_cislo) 
+
     while dec_cislo > 0:
         bin_pom = dec_cislo % 2 
         bin_cislo = str (bin_pom) + bin_cislo
         dec_cislo = dec_cislo // 2
     print ("Číslo převedené do binární soustavy je: ", bin_cislo)
+    
 else: 
     print ("Neplatný vstup. Zadej prosím celé kladné číslo.")
 print()
